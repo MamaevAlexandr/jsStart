f5bbe5d73c82f7a822ffca963492b6fa435cabf8
Alexandr Mamaev <twinyolo@yandex.ru>
2022-10-07 11:56:50 +0300

    jsStart

---
+++ js-2/js/script.js
@@ -0,0 +1,21 @@
+console.log('Учим') 
+console.log('JS')
+
+
+console.log('Учим JS');
+['Учим','Js'].forEach(alert)
+// Точка с запятой
+//вариант №1
+console.log('Учим JS')
+
+//Вариант№2
+console.log('JS')
+console.log('Учим')
+
+//Вариант№3
+console.log('Учим')
+console.log('JS')
+
+//Вариант№4
+console.log('Учим');
+console.log('JS');
